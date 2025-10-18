3956397f61865b94a6b05d3ecd18ce9c8b7ccb3a
RadimYassin <radimyassin20@gmail.com>
2025-10-18 22:18:09 +0100

    add Screenshot

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# TP-5-Int-gration-de-Spring-et-Hibernate" 
+![Screenshot](images/1.png)
+![Screenshot](images/2.png)
+![Screenshot](images/3.png)
+
--- src/test/java/test/config/TestConfig.java
+++ src/test/java/test/config/TestConfig.java
@@ -2,25 +2,27 @@ package test.config;
 
 import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@ComponentScan(basePackages = {"metier", "entities"})
 public class TestConfig {
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName("org.h2.Driver");
-        ds.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=MySQL");
-        ds.setUsername("sa");
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/base?useSSL=false&serverTimezone=UTC");
+        ds.setUsername("root");
         ds.setPassword("");
         return ds;
     }
@@ -31,8 +33,8 @@ public class TestConfig {
         sf.setDataSource(dataSource());
         sf.setPackagesToScan("entities");
         Properties props = new Properties();
-        props.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        props.put("hibernate.hbm2ddl.auto", "create-drop"); // crée la base à chaque test
+        props.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        props.put("hibernate.hbm2ddl.auto", "update"); // create-drop si tu veux tables temporaires
         props.put("hibernate.show_sql", "true");
         sf.setHibernateProperties(props);
         return sf;
